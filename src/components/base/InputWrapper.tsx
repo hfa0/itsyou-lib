e0e8828a72361c9972841e6e2036515a1e144b1c
hfa0 <fakhir.husein@gmail.com>
2025-03-12 10:33:10 +0100

    update

--- src/components/base/InputWrapper.tsx
+++ src/components/base/InputWrapper.tsx
@@ -15,6 +15,7 @@ const InputWrapper = ({
   error?: string;
   hideError?: boolean;
 }) => {
+  console.log(error);
   return (
     <div className={classNames('space-y-2', className)}>
       {label && (
