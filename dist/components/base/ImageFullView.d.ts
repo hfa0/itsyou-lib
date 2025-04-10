5cb04e96513521d5ecc2c493c5fbe24e8dc1813c
hfa0 <fakhir.husein@gmail.com>
2025-04-10 10:12:51 +0200

    update

--- dist/components/base/ImageFullView.d.ts
+++ dist/components/base/ImageFullView.d.ts
@@ -1,5 +1,6 @@
-declare const ImageFullView: ({ imageUrl, onClose, }: {
+declare const ImageFullView: ({ imageUrl, onClose, image, }: {
     imageUrl?: string;
     onClose?: () => void;
+    image?: any;
 }) => import("react/jsx-runtime").JSX.Element;
 export default ImageFullView;
